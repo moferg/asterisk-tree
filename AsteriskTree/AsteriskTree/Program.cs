5c46648d1cae8c4f93798a64275a517d9caaebd5
Marshall Ferguson <moferg01@louisville.edu>
2018-04-21 14:26:52 -0400

    Program now displays desired ouput to console

--- AsteriskTree/AsteriskTree/Program.cs
+++ AsteriskTree/AsteriskTree/Program.cs
@@ -10,16 +10,33 @@ namespace AsteriskTree
     {
         static void Main(string[] args)
         {
-            // Expected output:
-            //    *             Line 1, 4 spaces, 1 star, 4 spaces
-            //   ***            Line 2, 3 spaces, 3 stars, 3 spaces
-            //  *****           Line 3, 2 spaces, 5 stars, 2 spaces
-            // *******          Line 4, 1 space, 7 stars, 1 space   
-            //*********         Line 5, 9 stars
+            // Expected output: 
+            //    *             Line 1, 4 spaces, 1 star          spaceDiff 3      starDiff 0
+            //   ***            Line 2, 3 spaces, 3 stars         spaceDiff 1      starDiff 1
+            //  *****           Line 3, 2 spaces, 5 stars         spaceDiff -1     starDiff 2
+            // *******          Line 4, 1 space,  7 stars         spaceDiff -3     starDiff 3
+            //*********         Line 5, 0 space,  9 stars         spaceDiff -5     starDiff 4
 
             // Hints: 3 total loops, 1 outer and 2 inner
             // Outer loop goes from line to line
             // Inner loops print spaces and stars
+
+            for(int row = 0; row < 5; row++)
+            {
+                for(int spaces = 4; spaces > row; spaces--)
+                {
+                    Console.Write(" ");
+                }
+
+                for(int stars = -row + 1; stars <= row + 1; stars++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
