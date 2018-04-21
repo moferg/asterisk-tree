9e007c76425a87ea8634d3cc55c24f695fd7b185
Marshall Ferguson <moferg01@louisville.edu>
2018-04-21 13:21:16 -0400

    Outlined problem

--- AsteriskTree/AsteriskTree/Program.cs
+++ AsteriskTree/AsteriskTree/Program.cs
@@ -10,6 +10,16 @@ namespace AsteriskTree
     {
         static void Main(string[] args)
         {
+            // Expected output:
+            //    *             Line 1, 4 spaces, 1 star, 4 spaces
+            //   ***            Line 2, 3 spaces, 3 stars, 3 spaces
+            //  *****           Line 3, 2 spaces, 5 stars, 2 spaces
+            // *******          Line 4, 1 space, 7 stars, 1 space   
+            //*********         Line 5, 9 stars
+
+            // Hints: 3 total loops, 1 outer and 2 inner
+            // Outer loop goes from line to line
+            // Inner loops print spaces and stars
         }
     }
 }
